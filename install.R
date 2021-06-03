de204c72215f285414cdbd3668b9de060f979e50
Ryan Gosse <SoftwareMonk@users.noreply.github.com>
2021-06-03 14:29:28 -0300

    Update install.R

--- install.R
+++ install.R
@@ -45,8 +45,9 @@ install.packages("devtools")
 # GLATOS - acoustic telemetry package that does filtering, vis, array simulation, etc.
 #install.packages('remotes')
 #library(remotes)
-#install_url("https://gitlab.oceantrack.org/GreatLakes/glatos/repository/master/archive.zip",
-#            build_opts = c("--no-resave-data", "--no-manual"))
+#install_url("https://gitlab.oceantrack.org/GreatLakes/glatos/-/archive/master/glatos-master.zip",
+#          build_opts = c("--no-resave-data", "--no-manual"))
+
 
 # Sensible Working Directory Manager
 #install.packages("here")
